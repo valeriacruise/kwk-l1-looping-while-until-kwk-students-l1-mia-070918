726830f5991d0dabf4ae57ed1e4226ebf335d8b8
valeriacruise <valeriacruise22@gmail.com>
2018-07-10 19:09:36 +0000

    automatically backed up by learn

--- until.rb
+++ until.rb
@@ -1,9 +1,9 @@
-def wingardium_leviosa
-  wingardium_leviosa= 6
-  until wingardium_leviosa==10
+def using_until
+  levitation_force= 6
+  until levitation_force==10
   puts "Wingardium Leviosa"
-  wingardium_leviosa+= 1
+  levitation_force+= 1
   end
 end  
 
-wingardium_leviosa
+using_until
