5f1578217014b07599309b30684903a61b810f26
valeriacruise <valeriacruise22@gmail.com>
2018-07-10 19:02:36 +0000

    automatically backed up by learn

--- until.rb
+++
@@ -1,9 +0,0 @@
-def wingardium_leviosa
-  wingardium_leviosa= 6
-  while wingardium_leviosa< 10
-  puts "Wingardium Leviosa"
-  wingardium_leviosa+= 1
-  end
-end
-
-wingardium_leviosa
--- while.rb
+++ while.rb
@@ -1,1 +1,9 @@
 def wingardium_leviosa
+  wingardium_leviosa= 6
+  while wingardium_leviosa< 10
+  puts "Wingardium Leviosa"
+  wingardium_leviosa+= 1
+  end
+end
+
+wingardium_leviosa
